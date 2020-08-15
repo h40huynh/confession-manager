67f59ec21d2054c2e52207d155dcd77435a95dea
h40huynh <h40huynh@gmail.com>
2020-08-15 23:02:31 +0700

    Design home and authentication component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -19,46 +19,56 @@ const Home = () => {
   return (
     <main id="mainContent">
       <div className="container">
-        <div className="form-group">
-          <select
-            className="form-control custom-select bottom-border-only"
-            defaultValue={-1}
-            id="type"
-          >
-            <option value={-1} disabled>
-              Chọn thể loại
-            </option>
-            {categoryState.map((category, index) => (
-              <option key={index} value={index}>
-                {category}
+        <div className="mt-5">
+          <h3 className="mb-3 text-center">Gửi confession</h3>
+
+          <div className="form-group">
+            <select
+              className="form-control custom-select bottom-border-only"
+              defaultValue={-1}
+              id="type"
+            >
+              <option value={-1} disabled>
+                Chọn thể loại
               </option>
-            ))}
-          </select>
+              {categoryState.map((category, index) => (
+                <option key={index} value={index}>
+                  {category}
+                </option>
+              ))}
+            </select>
 
-          <small id="typeHelpBlock" className="form-text text-muted">
-            Hãy chọn thể loại mà bạn nghĩ bài viết của bạn thuộc về.
-          </small>
-        </div>
+            <small
+              id="typeHelpBlock"
+              className="form-text text-muted text-justify"
+            >
+              Hãy chọn thể loại mà bạn nghĩ bài viết của bạn thuộc về.
+            </small>
+          </div>
 
-        <div className="form-group">
-          <textarea
-            className="form-control bottom-border-only"
-            id="content"
-            rows="5"
-            onChange={onchangeContent}
-            placeholder="Nhập nội dung bài viết tại đây..."
-          ></textarea>
-          <small id="contentHelpBlock" className="form-text text-muted">
-            <b>Lưu ý:</b> sử dụng ngôn ngữ phù hợp, hạn chế nhắc tên cụ thể một
-            bạn nào đó để tránh gây phiền phức. Quan trọng nhất, viết tắt thì
-            một số quản trị viên sẽ xóa bài.
-          </small>
-        </div>
+          <div className="form-group">
+            <textarea
+              className="form-control bottom-border-only"
+              id="content"
+              rows="5"
+              onChange={onchangeContent}
+              placeholder="Nhập nội dung bài viết tại đây..."
+            ></textarea>
+            <small
+              id="contentHelpBlock"
+              className="form-text text-muted text-justify"
+            >
+              <b>Lưu ý:</b> sử dụng ngôn ngữ phù hợp, hạn chế nhắc tên cụ thể
+              một bạn nào đó để tránh gây phiền phức. Quan trọng nhất, viết tắt
+              thì một số quản trị viên sẽ xóa bài.
+            </small>
+          </div>
 
-        <div className="text-center">
-          <button className="btn btn-primary" onClick={sendConfession}>
-            Gửi confession
-          </button>
+          <div className="text-center">
+            <button className="btn btn-dark" onClick={sendConfession}>
+              Gửi bài viết
+            </button>
+          </div>
         </div>
       </div>
     </main>
